b6e3d5dec7e28d5f1c8fd8657b97aa8f1571fff5
wltks98 <wltks98@hanmail.net>
2022-10-24 15:35:12 +0900

    [#1] feat: UserMapper 구현

---
+++ src/main/java/com/numble/backend/user/domain/UserMapper.java
@@ -0,0 +1,17 @@
+package com.numble.backend.user.domain;
+
+import com.numble.backend.user.dto.UserResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(source = "name", target = "name") // 2
+    User ToEntity(UserResponse dto);
+
+    @Mapping(source = "name", target = "name")
+    UserResponse ToDto(User user);
+}
