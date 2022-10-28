369f4b81325bfc9467155864dd38a5553cf9b4a1
Jisan <79401359+wltks98@users.noreply.github.com>
2022-10-28 12:47:20 +0900

    [main] refactor: @Getter 삭제

--- src/main/java/com/numble/backend/BackendApplication.java
+++ src/main/java/com/numble/backend/BackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Getter
 public class BackendApplication {
 
 	public static void main(String[] args) {
