cd9cafeeaefe88622e91d7b1d9d329b373616615
wltks98 <wltks98@hanmail.net>
2022-11-01 15:48:36 +0900

    [#11] feat : imgUrl -> img, 유효성 검사 추가

--- src/main/java/com/numble/backend/user/dto/request/UserCreateRequest.java
+++ src/main/java/com/numble/backend/user/dto/request/UserCreateRequest.java
@@ -1,15 +1,30 @@
 package com.numble.backend.user.dto.request;
 
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class UserCreateRequest {
+	@NotNull
+	@Email
 	private String email;
+
+	@NotNull
 	private String password;
+
+	@NotNull
 	private String name;
+
+	@NotNull
 	private String phone;
+
+	@NotNull
 	private String nickname;
 
 }
