d21fe2f0ad6d08dd4fe54003a45696ba3dcadef5
wltks98 <wltks98@hanmail.net>
2022-11-01 15:26:47 +0900

    [#11] style : 빈 줄 제거

--- src/main/java/com/numble/backend/user/exception/EmailExistsException.java
+++ src/main/java/com/numble/backend/user/exception/EmailExistsException.java
@@ -7,7 +7,6 @@ import com.numble.backend.common.exception.BusinessException;
 public class EmailExistsException extends BusinessException {
 
 	private static final String CLIENT_MESSAGE = "이메일이 존재합니다.";
-
 	public EmailExistsException() {
 		super(CLIENT_MESSAGE, HttpStatus.BAD_REQUEST);
 	}
--- src/main/java/com/numble/backend/user/exception/UserNotFoundException.java
+++ src/main/java/com/numble/backend/user/exception/UserNotFoundException.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 
 public class UserNotFoundException extends BusinessException {
 	private static final String CLIENT_MESSAGE = "사용자가 존재하지 않습니다.";
-
 	public UserNotFoundException() {
 		super(CLIENT_MESSAGE, HttpStatus.BAD_REQUEST);
 	}
