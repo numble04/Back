71d35c856bae55239aa6c94f08c7df5e2ff47890
idwooin <idwooin@naver.com>
2022-11-02 18:20:41 +0900

    [#17] fix: exception 내용이 한글로 출력되지 않아 영어로 수정

--- src/main/java/com/numble/backend/common/exception/ExceptionCode.java
+++ src/main/java/com/numble/backend/common/exception/ExceptionCode.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionCode {
-    WRONG_TYPE_TOKEN("1000","토큰 타입이 잘못됨"),
-    WRONG_TOKEN("1005","토큰 잘못됨"),
-    EXPIRED_TOKEN("1001","토큰이 만료됨"),
-    UNSUPPORTED_TOKEN("1002","지원하지 않는 토큰"),
-    ACCESS_DENIED("403","접근 금지"),
-    UNKNOWN_ERROR("9999", "에러를 알 수 없음");
+    WRONG_TYPE_TOKEN("1000","token type is wrong"),
+    WRONG_TOKEN("1005","token is wrong"),
+    EXPIRED_TOKEN("1001","token is expired"),
+    UNSUPPORTED_TOKEN("1002","token unavailable"),
+    NO_TOKEN("1010","no token"),
+    ACCESS_DENIED("403","access denied"),
+    UNKNOWN_ERROR("9999", "error is unknown");
 
     private final String code;
     private final String message;
