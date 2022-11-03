766fb239e98b179e434aab179b7397e8a06f94e9
wltks98 <wltks98@hanmail.net>
2022-11-03 14:00:18 +0900

    [#27] refactor : 공통 response ResponseDto 반환으로 리팩토링

--- src/main/java/com/numble/backend/post/presentation/PostController.java
+++ src/main/java/com/numble/backend/post/presentation/PostController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.numble.backend.common.config.security.CustomUserDetails;
+import com.numble.backend.common.dto.ResponseDto;
 import com.numble.backend.post.application.PostService;
 import com.numble.backend.post.domain.PostType;
 import com.numble.backend.post.dto.request.PostCreateRequest;
@@ -33,54 +34,50 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PostController {
 
-    private final PostService postService;
-
+	private final PostService postService;
 
 	@PostMapping
 	public ResponseEntity<Void> save(@AuthenticationPrincipal CustomUserDetails customUserDetails,
 
 		@RequestBody @Valid PostCreateRequest postRequest) {
 
-	    final Long id = postService.save(customUserDetails, postRequest);
+		final Long id = postService.save(customUserDetails, postRequest);
 
-	    return ResponseEntity.created(URI.create("/api/posts/"+id)).build();
+		return ResponseEntity.created(URI.create("/api/posts/" + id)).build();
 	}
 
 	@PostMapping("/upload")
 	public String uploadFile(
 		@RequestPart(value = "file") MultipartFile multipartFile) {
-		Long postId=Long.valueOf(1);
+		Long postId = Long.valueOf(1);
 		return postService.uploadFile(multipartFile, postId);
 	}
 
 	@GetMapping
-	public ResponseEntity<PostResponses> findAll(@RequestParam("type") PostType type) {
-		final PostResponses postResponses = postService.findAll(type);
-
-		return ResponseEntity.ok(postResponses);
+	public ResponseEntity<ResponseDto> findAll(@RequestParam("type") PostType type) {
+		ResponseDto responseDto = ResponseDto.of(postService.findAll(type));
+		return ResponseEntity.ok(responseDto);
 	}
 
 	@GetMapping("/2")
-	public ResponseEntity<PostResponses> findAllByUserId(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+	public ResponseEntity<ResponseDto> findAllByUserId(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
 
-		final PostResponses postResponses = postService.findAllByUserId(customUserDetails);
-
-		return ResponseEntity.ok(postResponses);
+		ResponseDto responseDto = ResponseDto.of(postService.findAllByUserId(customUserDetails));
+		return ResponseEntity.ok(responseDto);
 	}
 
 	@GetMapping("/3/{postId}")
-	public ResponseEntity<PostResponse> findById(@PathVariable final Long postId) {
+	public ResponseEntity<ResponseDto> findById(@PathVariable final Long postId) {
 
-		final PostResponse postResponse = postService.findById(postId);
-
-		return ResponseEntity.ok(postResponse);
+		ResponseDto responseDto = ResponseDto.of(postService.findById(postId));
+		return ResponseEntity.ok(responseDto);
 	}
 
 	@PutMapping("/{postId}")
 	public ResponseEntity<Void> updateById(@AuthenticationPrincipal CustomUserDetails customUserDetails,
 		@PathVariable final Long postId, @RequestBody PostUpdateRequest postUpdateRequest) {
 
-		postService.updateById(customUserDetails, postId,postUpdateRequest);
+		postService.updateById(customUserDetails, postId, postUpdateRequest);
 
 		return ResponseEntity.noContent().build();
 	}
@@ -89,12 +86,10 @@ public class PostController {
 	public ResponseEntity<Void> updateLikeById(@AuthenticationPrincipal CustomUserDetails customUserDetails,
 		@PathVariable final Long postId, @RequestBody PostUpdateRequest postUpdateRequest) {
 
-		postService.updateLikeById(customUserDetails, postId,postUpdateRequest);
+		postService.updateLikeById(customUserDetails, postId, postUpdateRequest);
 
 		return ResponseEntity.noContent().build();
 	}
-
-
 
 	@DeleteMapping("/{postId}")
 	public ResponseEntity<Void> updateById(@AuthenticationPrincipal CustomUserDetails customUserDetails,
