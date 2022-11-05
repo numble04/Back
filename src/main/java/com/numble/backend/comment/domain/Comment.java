d39e2f1c8f39f136ff4a92e4360d7aa9f2d48350
wltks98 <wltks98@hanmail.net>
2022-11-05 21:14:38 +0900

    [#44] style : import 수정

--- src/main/java/com/numble/backend/comment/domain/Comment.java
+++ src/main/java/com/numble/backend/comment/domain/Comment.java
@@ -2,6 +2,7 @@ package com.numble.backend.comment.domain;
 
 import static javax.persistence.FetchType.LAZY;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
