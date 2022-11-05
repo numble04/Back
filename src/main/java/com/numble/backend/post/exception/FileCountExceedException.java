27e4aa852d5be23483d2b9f2dbfd61694efc740e
wltks98 <wltks98@hanmail.net>
2022-11-05 21:11:46 +0900

    [#44] feat : 파일 업로드 예외 생성

---
+++ src/main/java/com/numble/backend/post/exception/FileCountExceedException.java
@@ -0,0 +1,13 @@
+package com.numble.backend.post.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.numble.backend.common.exception.business.BusinessException;
+
+public class FileCountExceedException extends BusinessException {
+	private static final String CLIENT_MESSAGE = "이미지 업로드 갯수를 초과했습니다";
+
+	public FileCountExceedException() {
+		super(CLIENT_MESSAGE, HttpStatus.BAD_REQUEST);
+	}
+}
