a229d883d99faa3e5fd1d2822eab086882546b7c
wltks98 <wltks98@hanmail.net>
2022-11-07 21:41:01 +0900

    [#48] style : 공백 추가

--- src/main/java/com/numble/backend/post/dto/request/PostCreateRequest.java
+++ src/main/java/com/numble/backend/post/dto/request/PostCreateRequest.java
@@ -16,8 +16,10 @@ public class PostCreateRequest {
 	@NotBlank(message = "제목을 입력해 주세요")
 	@Size(min = 0, max=50, message = "제목은 50자 이내로 입력해 주세요")
 	private String title;
+
 	@NotBlank(message = "내용을 입력해 주세요")
 	private String content;
+
 	@NotNull(message = "게시판 유형을 확인해 주세요")
 	private PostType type;
 }
