9a653915f0fd1e9f4a97eea6d5ccc2699f29d672
wltks98 <wltks98@hanmail.net>
2022-11-07 21:40:25 +0900

    [#48] feat : 게시글 좋아요 기능 구현

--- src/main/java/com/numble/backend/post/application/PostService.java
+++ src/main/java/com/numble/backend/post/application/PostService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -22,11 +23,13 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.numble.backend.common.config.security.CustomUserDetails;
 import com.numble.backend.common.utils.S3Utils;
 import com.numble.backend.post.domain.Image;
+import com.numble.backend.post.domain.PostLike;
 import com.numble.backend.post.domain.repository.ImageRepository;
 import com.numble.backend.post.domain.Post;
 import com.numble.backend.post.domain.PostType;
 import com.numble.backend.post.domain.mapper.PostCreateMapper;
 import com.numble.backend.post.domain.mapper.PostMapper;
+import com.numble.backend.post.domain.repository.PostLikeRepository;
 import com.numble.backend.post.domain.repository.PostRepository;
 
 import com.numble.backend.post.dto.request.PostCreateRequest;
@@ -46,11 +49,12 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PostService {
+	private final UserRepository userRepository;
 	private final PostRepository postRepository;
-
+	private final PostLikeRepository postLikeRepository;
 	private final ImageRepository imageRepository;
 	private final AmazonS3Client amazonS3Client;
-	private final UserRepository userRepository;
+
 	@Value("${cloud.aws.s3.bucket}")
 	private String bucketName;
 
@@ -75,6 +79,16 @@ public class PostService {
 		return postResponses;
 	}
 
+	@Transactional
+	public PostOneResponse findById(Long postId, CustomUserDetails customUserDetails) {
+		PostOneResponse postOneResponse = postRepository.findOnePostById(postId, customUserDetails.getId())
+			.orElseThrow(() -> new PostNotFoundException());
+
+		postOneResponse.setImages(
+			imageRepository.findByPostId(postId).stream().map(i -> i.getUrl()).collect(Collectors.toList()));
+		return postOneResponse;
+	}
+
 	public List<PostResponse> findAllByUserId(CustomUserDetails customUserDetails) {
 
 		// User user = userRepository.findById(customUserDetails.getId())
@@ -89,14 +103,51 @@ public class PostService {
 		return null;
 	}
 
+
+
 	@Transactional
-	public PostOneResponse findById(Long postId, CustomUserDetails customUserDetails) {
-		PostOneResponse postOneResponse = postRepository.findOnePostById(postId, customUserDetails.getId())
+	public void updateById(CustomUserDetails customUserDetails, Long postId, PostUpdateRequest postUpdateRequest
+		, List<MultipartFile> multipartFiles) {
+
+		Post post = postRepository.findById(postId)
 			.orElseThrow(() -> new PostNotFoundException());
 
-		postOneResponse.setImages(
-			imageRepository.findByPostId(postId).stream().map(i -> i.getUrl()).collect(Collectors.toList()));
-		return postOneResponse;
+		List<Image> images = post.getImages();
+		imageRepository.deleteInBatch(images);
+
+		uploadFiles(multipartFiles, post);
+
+		post.updatePost(postUpdateRequest, customUserDetails.getId());
+	}
+
+	@Transactional
+	public void deleteById(CustomUserDetails customUserDetails, Long postId) {
+		Post post = postRepository.findById(postId)
+			.orElseThrow(() -> new PostNotFoundException());
+
+		post.validateMemberIsAuthor(customUserDetails.getId());
+
+		postRepository.deleteById(postId);
+	}
+
+	@Transactional
+	public void updateLikeById(CustomUserDetails customUserDetails, Long postId) {
+		Post post = postRepository.findById(postId)
+			.orElseThrow(() -> new PostNotFoundException());
+
+		User user = userRepository.findById(customUserDetails.getId())
+			.orElseThrow(() -> new UserNotFoundException());
+
+		Optional<PostLike> postLike = postLikeRepository.findByPostIdAndUserId(postId,
+			customUserDetails.getId());
+
+		if (postLike.isPresent()) {
+			postLikeRepository.delete(postLike.get());
+		} else {
+			PostLike postLike1 = new PostLike(user, post);
+			postLikeRepository.save(postLike1);
+		}
+
 	}
 
 	@Transactional
@@ -115,32 +166,6 @@ public class PostService {
 
 			imageRepository.save(image);
 		}
-	}
-
-	@Transactional
-	public void updateById(CustomUserDetails customUserDetails, Long postId, PostUpdateRequest postUpdateRequest) {
-
-		Post post = postRepository.findById(postId)
-			.orElseThrow(() -> new PostNotFoundException());
-
-		post.updateContent(postUpdateRequest.getContent(), customUserDetails.getId());
-	}
-
-	@Transactional
-	public void deleteById(CustomUserDetails customUserDetails, Long postId) {
-		Post post = postRepository.findById(postId)
-			.orElseThrow(() -> new PostNotFoundException());
-
-		post.validateMemberIsAuthor(customUserDetails.getId());
-
-		postRepository.deleteById(postId);
-	}
-
-	@Transactional
-	public void updateLikeById(CustomUserDetails customUserDetails, Long postId, PostUpdateRequest postUpdateRequest) {
-		Post post = postRepository.findById(postId)
-			.orElseThrow(() -> new PostNotFoundException());
-
 	}
 
 }
