b53f0d204f1a6c9585698574726343fbb890e57c
idwooin <idwooin@naver.com>
2022-11-08 17:00:32 +0900

    [#53] refactor: dto 유효성 검사 추가

--- src/main/java/com/numble/backend/user/dto/request/UserLoginRequest.java
+++ src/main/java/com/numble/backend/user/dto/request/UserLoginRequest.java
@@ -1,5 +1,7 @@
 package com.numble.backend.user.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import lombok.Builder;
@@ -8,8 +10,12 @@ import lombok.Getter;
 @Getter
 @Builder
 public class UserLoginRequest {
-	@NotNull
+
+	@NotBlank(message = "이메일은 빈 칸일 수 없습니다")
+	@Email(message = "이메일 형식이 맞지 않습니다")
 	private String email;
-	@NotNull
+
+	@NotBlank(message = "비밀번호는 빈 칸일 수 없습니다")
 	private String password;
+
 }
