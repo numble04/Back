f225e2cf38b5950558125f2f3d6ab768235a0bae
idwooin <idwooin@naver.com>
2022-11-08 17:00:14 +0900

    [#53] refactor: access token 재발급 시 user 정보도 return하도록 추가, 유효성 검사 추가

--- src/main/java/com/numble/backend/user/application/AuthService.java
+++ src/main/java/com/numble/backend/user/application/AuthService.java
@@ -1,16 +1,26 @@
 package com.numble.backend.user.application;
 
+import java.util.Optional;
+
 import com.numble.backend.common.config.jwt.JwtTokenUtil;
+import com.numble.backend.common.config.security.CustomUserDetailsService;
 import com.numble.backend.common.domain.refresh.RefreshToken;
 import com.numble.backend.common.domain.refresh.RefreshTokenRedisRepository;
 import com.numble.backend.user.domain.Token;
-import com.numble.backend.user.domain.mapper.UserLoginMapper;
+import com.numble.backend.user.domain.User;
+import com.numble.backend.user.domain.UserRepository;
+import com.numble.backend.user.domain.mapper.UserMapper;
+import com.numble.backend.user.domain.mapper.UserTokenMapper;
 import com.numble.backend.user.dto.response.UserTokenResponse;
+import com.numble.backend.user.exception.RefreshTokenNotFoundException;
+import com.numble.backend.user.exception.TokenErrorException;
+import com.numble.backend.user.exception.TokenNotExistsException;
 import com.numble.backend.user.exception.UserNotFoundException;
 
 import io.jsonwebtoken.io.DecodingException;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -22,25 +32,40 @@ public class AuthService {
 
 	private final JwtTokenUtil jwtTokenUtil;
 	private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+	private final UserRepository userRepository;
+	private final CustomUserDetailsService customUserDetailsService;
 
 	@Transactional
 	public UserTokenResponse reissue(String refreshToken) {
 		checkRefresh(refreshToken);
-		String username = jwtTokenUtil.getUsername(refreshToken);
-		RefreshToken refreshToken1 = refreshTokenRedisRepository.findById(username)
-			.orElseThrow(() -> new UserNotFoundException());
+		String id = jwtTokenUtil.getUsername(refreshToken);
+		UserDetails userDetails = customUserDetailsService.loadUserByUsername(id);
+		validateRefreshToken(refreshToken,userDetails);
 
-		Token t = Token.builder()
-			.accessToken(jwtTokenUtil.generateAccessToken(username))
+		RefreshToken refreshToken1 = refreshTokenRedisRepository.findById(id)
+			.orElseThrow(() -> new RefreshTokenNotFoundException());
+
+		Token token = Token.builder()
+			.accessToken(jwtTokenUtil.generateAccessToken(id))
 			.refreshToken(refreshToken1.getRefreshToken())
 			.build();
 
-		return UserLoginMapper.INSTANCE.toDto(t);
+		User user = userRepository.findById(Long.parseLong(id))
+			.orElseThrow(() -> new UserNotFoundException());
+
+
+		return UserTokenMapper.INSTANCE.toDto(token, UserMapper.INSTANCE.toDto(user));
 	}
 
 	private void checkRefresh(String refreshToken) {
 		if (!(StringUtils.hasText(refreshToken))) {
-			throw new DecodingException("토큰 내용 없음");
+			throw new TokenNotExistsException();
+		}
+	}
+
+	private void validateRefreshToken(String refreshToken,UserDetails userDetails) {
+		if (!jwtTokenUtil.validateToken(refreshToken, userDetails)) {
+			throw new TokenErrorException();
 		}
 	}
 
