573e16fc10e20cfaf464b6f2c8969dadeb99126a
idwooin <idwooin@naver.com>
2022-11-10 22:53:27 +0900

    [#70] feat: game api 추가 (game 조회, 상세 조회, 검색 조회)

--- src/main/java/com/numble/backend/game/application/GameService.java
+++ src/main/java/com/numble/backend/game/application/GameService.java
@@ -1,16 +1,17 @@
 package com.numble.backend.game.application;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.numble.backend.game.domain.GameRepository;
+import com.numble.backend.game.domain.repository.GameRepository;
+import com.numble.backend.game.domain.mapper.GetDetailGameResponseMapper;
 import com.numble.backend.game.domain.mapper.GetGameResponseMapper;
+import com.numble.backend.game.dto.response.GetDetailGameResponse;
 import com.numble.backend.game.dto.response.GetGameResponse;
+import com.numble.backend.game.exception.GameNotFoundException;
+import com.numble.backend.game.exception.GameTypeErrorException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,10 +21,73 @@ import lombok.RequiredArgsConstructor;
 public class GameService {
 	private final GameRepository gameRepository;
 
-	public Page<GetGameResponse> findAll(Pageable pageable) {
-		Page<GetGameResponse> responses = gameRepository.findAll(pageable)
-			.map(GetGameResponseMapper.INSTANCE::toDto);
+	public Slice<GetGameResponse> findAll(Pageable pageable, String type) {
+
+		if (type.equals("오름차순")) {
+			Slice<GetGameResponse> responses = gameRepository.findByOrderByTitleAsc(pageable)
+				.map(GetGameResponseMapper.INSTANCE::toDto);
+
+			return responses;
+
+		} else if (type.equals("평점순")) {
+			Slice<GetGameResponse> responses = gameRepository.findByOrderByRateAsc(pageable)
+				.map(GetGameResponseMapper.INSTANCE::toDto);
+
+			return responses;
+
+		} else if (type.equals("난이도순")) {
+			Slice<GetGameResponse> responses = gameRepository.findByOrderByLevelAsc(pageable)
+				.map(GetGameResponseMapper.INSTANCE::toDto);
+
+			return responses;
+
+		} else if (type.equals("내림차순")) {
+			Slice<GetGameResponse> responses = gameRepository.findByOrderByTitleDesc(pageable)
+				.map(GetGameResponseMapper.INSTANCE::toDto);
+
+			return responses;
+
+		} else {
+			throw new GameTypeErrorException();
+		}
+	}
+
+	public Slice<GetGameResponse> findAllBySearch(Pageable pageable, String title, String type) {
+		if (type.equals("오름차순")) {
+			Slice<GetGameResponse> responses = gameRepository.findByTitleLikeOrderByTitleAsc(pageable,title)
+				.map(GetGameResponseMapper.INSTANCE::toDto);
+
+			return responses;
+
+		} else if (type.equals("평점순")) {
+			Slice<GetGameResponse> responses = gameRepository.findByTitleLikeOrderByRateAsc(pageable,title)
+				.map(GetGameResponseMapper.INSTANCE::toDto);
+
+			return responses;
+
+		} else if (type.equals("난이도순")) {
+			Slice<GetGameResponse> responses = gameRepository.findByTitleLikeOrderByLevelAsc(pageable,title)
+				.map(GetGameResponseMapper.INSTANCE::toDto);
+
+			return responses;
+
+		} else if (type.equals("내림차순")) {
+			Slice<GetGameResponse> responses = gameRepository.findByTitleLikeOrderByTitleDesc(pageable,title)
+				.map(GetGameResponseMapper.INSTANCE::toDto);
+
+			return responses;
+
+		} else {
+			throw new GameTypeErrorException();
+		}
+	}
+
+	public GetDetailGameResponse findById(Long id) {
+		GetDetailGameResponse responses = GetDetailGameResponseMapper.INSTANCE.toDto(
+			gameRepository.findById(id)
+			.orElseThrow(() ->  new GameNotFoundException()));
 
 		return responses;
 	}
+
 }
