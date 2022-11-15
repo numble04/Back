d681dac90c61901e579c3bdeb96a07b7535244df
idwooin <idwooin@naver.com>
2022-11-15 20:03:53 +0900

    [#70] refactor: queryprojection 추가

--- src/main/java/com/numble/backend/game/dto/response/GetGameResponse.java
+++ src/main/java/com/numble/backend/game/dto/response/GetGameResponse.java
@@ -1,5 +1,6 @@
 package com.numble.backend.game.dto.response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -7,12 +8,16 @@ import javax.persistence.Column;
 import javax.persistence.OneToMany;
 
 import com.numble.backend.game.domain.Theme;
+import com.querydsl.core.annotations.QueryProjection;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@Builder
+@Setter
+@NoArgsConstructor
 public class GetGameResponse {
 	private Long id;
 	private String title;
@@ -22,5 +27,18 @@ public class GetGameResponse {
 	private String age;
 	private Double rate;
 	private Double level;
-	private List<Theme> themeList;
+
+	private List<ThemeResponse> themeList = new ArrayList<>();
+
+	@QueryProjection
+	public GetGameResponse(Long id,String title,String img,String capacity,String time,String age,Double rate,Double level) {
+		this.id = id;
+		this.title = title;
+		this.img = img;
+		this.capacity = capacity;
+		this.time = time;
+		this.age = age;
+		this.rate = rate;
+		this.level = level;
+	}
 }
--- src/main/java/com/numble/backend/game/dto/response/GetReviewsResponse.java
+++ src/main/java/com/numble/backend/game/dto/response/GetReviewsResponse.java
@@ -2,18 +2,23 @@ package com.numble.backend.game.dto.response;
 
 import static javax.persistence.FetchType.LAZY;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.numble.backend.game.domain.Game;
 import com.numble.backend.user.domain.User;
+import com.querydsl.core.annotations.QueryProjection;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Getter
+@NoArgsConstructor
 public class GetReviewsResponse {
 
 	private Long id;
@@ -22,6 +27,23 @@ public class GetReviewsResponse {
 
 	private Double rate;
 
-	private String user;
+	private String nickname;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime createDate;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime updateDate;
+
+	@QueryProjection
+	public GetReviewsResponse(Long id,String content,Double rate,String nickname,
+		LocalDateTime createDate, LocalDateTime updateDate) {
+		this.id = id;
+		this.content = content;
+		this.rate = rate;
+		this.nickname = nickname;
+		this.createDate = createDate;
+		this.updateDate = updateDate;
+	}
 
 }
