639a769ead4008296e5255a3d58c51f1cbb95953
idwooin <idwooin@naver.com>
2022-11-17 22:35:19 +0900

    [#109] feat: redis에 60초 정도 토큰 유지 cache 추가

--- src/main/java/com/numble/backend/common/config/security/CustomUserDetailsService.java
+++ src/main/java/com/numble/backend/common/config/security/CustomUserDetailsService.java
@@ -1,11 +1,13 @@
 package com.numble.backend.common.config.security;
 
+import com.numble.backend.common.config.redis.CacheKey;
 import com.numble.backend.user.domain.UserRepository;
 import com.numble.backend.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Cacheable(value = CacheKey.USER, key = "#username", unless = "#result == null")
     public UserDetails loadUserByUsername(String username) throws UserNotFoundException {
         return CustomUserDetails.of(
                 userRepository.findById(Long.parseLong(username))
