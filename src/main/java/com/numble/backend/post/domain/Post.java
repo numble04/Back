b65b4b0b8bc0bd41e183e6ddcabedaf7f5eb8979
wltks98 <wltks98@hanmail.net>
2022-11-19 11:02:31 +0900

    [#121] fix : batchSize 설정으로 n+1 문제 해결

--- src/main/java/com/numble/backend/post/domain/Post.java
+++ src/main/java/com/numble/backend/post/domain/Post.java
@@ -14,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
@@ -57,6 +58,7 @@ public class Post extends BaseEntity {
 	@JoinColumn(name = "userId")
 	private User user;
 
+	@BatchSize(size = 100)
 	@OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
 	private List<Image> images;
 
