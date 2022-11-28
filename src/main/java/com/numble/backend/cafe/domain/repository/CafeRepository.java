1c1be4ce3282b02981cce526fa319b0b92358675
idwooin <idwooin@naver.com>
2022-11-28 15:46:25 +0900

    [#150] refactor: keyword blank 시 empty slice 반환

--- src/main/java/com/numble/backend/cafe/domain/repository/CafeRepository.java
+++ src/main/java/com/numble/backend/cafe/domain/repository/CafeRepository.java
@@ -1,8 +1,10 @@
-package com.numble.backend.cafe.domain;
+package com.numble.backend.cafe.domain.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.numble.backend.cafe.domain.Cafe;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
 
